20bf44a7bf35b38f5aa9d8bf37733e91b931216d
Tech_Query <shiy007@qq.com>
2016-02-26 11:53:52 +0800

    【重构】基于 $.TreeView()（iQuery+ v0.9）精简正文多级目录导航栏的代码

--- core/javascript/index.js
+++ core/javascript/index.js
@@ -33,61 +33,33 @@
         });
     }
 
-    var iMainNav = $.ListView('#Main_Nav',  function ($_Item, iValue) {
+    var iMainNav = $.TreeView(
+            $.ListView('#Main_Nav',  function ($_Item, iValue) {
 
-            $('a[rel="nofollow"]', $_Item[0]).text(iValue.text)[0].href =
-                '#' + iValue.id;
-            $_Item.attr('title', iValue.text);
+                $('a[rel="nofollow"]', $_Item[0]).text(iValue.text)[0].href =
+                    '#' + iValue.id;
+                $_Item.attr('title', iValue.text);
+            }),
+            'list',
+            function () {
+                this.$_View.attr('class', '');
+            },
+            function () {
+                var $_Target = $(arguments[0].target);
 
-            if (! iValue.list)  return;
-
-            this.fork($_Item).clear().render( iValue.list )
-                .$_View.attr('class', '');
-        }),
+                if ( $_Target.is('a[rel="nofollow"]') )
+                    return (
+                        '*[id="'  +  $_Target.attr('href').slice(1)  +  '"]'
+                    );
+            }
+        ),
         $_MainView = $('#Main_View');
 
-    iMainNav.$_View.click(function () {
-        var $_Target = $(arguments[0].target);
-
-        if ( $_Target.is('a[rel="nofollow"]') ) {
-            $_MainView.scrollTo(
-                '*[id="'  +  $_Target.attr('href').slice(1)  +  '"]'
-            );
-            return false;
-        }
-    });
-
     var $_Body = $(DOM.body).swipe(function () {
-            iMainNav.$_View[
+            iMainNav.unit.$_View[
                 (arguments[0].deltaX < 0)  ?  'show'  :  'hide'
             ]();
         });
-
-    function Content_Tree($_Tree, iCallback) {
-        var iTree = [ ];
-
-        for (
-            var  i = 0,  _Tree_ = iTree,  _Level_ = 0,  _Parent_;
-            i < $_Tree.length;
-            i++
-        ) {
-            if (i > 0)
-                _Level_ = $_Tree[i].tagName[1]  -  $_Tree[i - 1].tagName[1];
-
-            if (_Level_ > 0)
-                _Tree_ = _Tree_.slice(-1)[0].list = $.extend([ ], {
-                    parent:    _Tree_
-                });
-            else if (_Level_ < 0) {
-                _Parent_ = _Tree_.parent;
-                delete _Tree_.parent;
-                _Tree_ = _Parent_;
-            }
-            _Tree_.push( iCallback.call($_Tree[i]) );
-        }
-
-        return iTree;
-    }
 
     $_MainView.on('pageRender',  function (iEvent, This_Page, Prev_Page, iData) {
         var _TP_ = $.fileName(This_Page.HTML),
@@ -121,15 +93,19 @@
         });
     }).on('pageReady',  function () {
 
-        iMainNav.clear().render(
-            Content_Tree($('h1, h2, h3', this),  function () {
+        iMainNav.bind(
+            $('h1, h2, h3', this),
+            function ($_A, $_B) {
+                return  $_B.tagName[1] - $_A.tagName[1];
+            },
+            function () {
                 if (this.id == '-')  this.id = $.uuid('Header');
 
                 return {
                     id:      this.id,
                     text:    $(this).text()
                 };
-            })
+            }
         );
     }).WebApp();
 
